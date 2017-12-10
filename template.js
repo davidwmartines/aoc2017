65a5a2506e4963cc9572169e0c0d5afde80f6376
David W Martines <d5172@yahoo.com>
2017-12-09 20:06:35 -0600

    Update template.

--- template.js
+++ template.js
@@ -6,20 +6,29 @@ function solve(input){
 
 }
 
-const runTest = function(input, expected){
-  test(`solve`, assert => {
-    const msg = `solve with ${input} should return ${expected}`;
-    const value = solve(input);
-    assert.equal(value, expected, msg);
-    assert.end();
-  });
+function runTests(){
+  const runTest = function(input, expected){
+    test(`solve`, assert => {
+      const msg = `solve with ${input} should return ${expected}`;
+      const value = solve(input);
+      assert.equal(value, expected, msg);
+      assert.end();
+    });
+  }
+  
+  runTest('1212', 6);
 }
 
-runTest('1212', 6);
+function solvePuzzle() {
+  const puzzleInput = fs.readFileSync('./day0.txt', {
+    encoding: 'utf8'
+  });
+
+  console.log('* * ANSWER * *');
+  console.log(solve(puzzleInput));
+  console.log('* * * * * * * *');
+}
 
 
-const puzzleInput = fs.readFileSync('./day01.txt', {
-  encoding: 'utf8'
-});
-
-console.log('answer', solve(puzzleInput));
+runTests();
+//solvePuzzle();
